9c9e9644a0208ade0bdf410733e2b093bc735d78
cheez0idgit <93955021+Chee0id@users.noreply.github.com>
2022-02-15 18:57:04 -0500

    GET POST PUT DELETE! RESTFUL API! CRUDgit commit -m!

--- controllers/entries.js
+++ controllers/entries.js
@@ -1,31 +1,54 @@
-const { UserEntries } = require('../models');
-const router = require('express').Router();
+const { UserEntries } = require("../models");
+const router = require("express").Router();
 
-// primary Trying to make routes modularized; 
+// primary Trying to make routes modularized;
 
- router.get('/alldata', async(req, res) => {
+router.get("/allData", async (req, res) => {
+	UserEntries.findAll().then((userData) => {
+		console.log("Gotta Catch 'Em All!");
+		res.json(userData);
+	});
+});
+
+router.post("/newEntry", async (req, res) => {
+	UserEntries.create({
+		mood: 3,
+		episode: true,
+		text: "This is another new entry",
+		makePrivate: true,
+	}).then((newEntry) => {
+		console.log("making a new entry");
+	});
+});
+
+router.put("/editEntry", async (req, res) => {
+  UserEntries.update({ 
+    text: 'a very different text update RETURNS'
+   },
+   {
+    where: {
+      createdAt: "2022-02-15T23:23:28.000Z"
+    }
+	}).then(
     UserEntries.findAll().then((userData) => {
-      console.log("Gotta Catch 'Em All!");
-      res.json(userData)
-    });    
-  })
+      console.log("showing them all to you with updates!");
+      res.json(userData)})
+    );
+});
 
 
 
-  router.post('/thingy', async(req, res) => {
-    UserEntries.create({
-      mood: 3,
-      episode: true,
-      text: 'This is a new entry',
-      makePrivate: true
-    }).then((newEntry) => {
-      console.log("making a new entry");
-      res.json(newEntry)
-    });    
-  })
-
-
+router.delete("/deleteEntry", async (req, res) => {
+	UserEntries.destroy(
+   {
+    where: {
+      id: 2
+    }
+	}).then(function(){
+       console.log('Deleted successfully');
+     }, function(err){
+      console.log(err); 
+  });
+});
 
 module.exports = router;
-
-
