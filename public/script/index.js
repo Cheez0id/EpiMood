b56a5dfdc95e1281691b54590b708eeedda12d86
cheez0idgit <93955021+Chee0id@users.noreply.github.com>
2022-02-16 17:32:38 -0500

    added form with radios with values 1-6

--- public/script/index.js
+++ public/script/index.js
@@ -1,7 +1,7 @@
 
 
 //this is just testing connection between this file and the index.html. how do I make this talk to tbe backend?
-document.getElementById("moodNumber").addEventListener("click", function (event) {
+document.getElementById("submitMood").addEventListener("click", function (event) {
 	event.preventDefault();
 	console.log("you clicked the form");
 	});
