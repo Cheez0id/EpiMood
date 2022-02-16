802df4f0929f0d3efba2f5f9d314097831861e78
cheez0idgit <93955021+Chee0id@users.noreply.github.com>
2022-02-15 20:52:04 -0500

    working on variables for dynamic queries

--- controllers/entries.js
+++ controllers/entries.js
@@ -1,13 +1,43 @@
 const { UserEntries } = require("../models");
 const router = require("express").Router();
 
-// Routes for a RESTful API
+//setting up variables where user input will be put into the queries 
+//TODO: checkbox for episodes
+let episodes = {where: {
+  episode: true,
+  }}
+//TODO:user should be able to enter in a number (1-6)  
+let mood = {where: {
+  mood: 3,
+  }}  
 
-//shows all of the data in JSON
+
+//TODO: need to get values from user inputs from forms for the POST to create new entries
+
+//--------------------
+// Routes for a RESTful API!!
+
+//shows all of the userentries data in JSON
 router.get("/allData", async (req, res) => {
-	UserEntries.findAll().then((userData) => {
+	UserEntries.findAll().then((entryData) => {
 		console.log("Gotta Catch 'Em All!");
-		res.json(userData);
+		res.json(entryData);
+	});
+});
+
+//shows a list of entries with episodes
+router.get("/allData/episodes", async (req, res) => {
+	UserEntries.findAll(episodes).then((entryData) => {
+		console.log("Found episodes!");
+		res.json(entryData);
+	});
+});
+
+//shows a list of entries with moods
+router.get("/allData/mood", async (req, res) => {
+	UserEntries.findAll(mood).then((entryData) => {
+		console.log("Found episodes!");
+		res.json(entryData);
 	});
 });
 
@@ -26,15 +56,15 @@ router.post("/newEntry", async (req, res) => {
 //Updates a specific entry with a specified value (TODO: make it so that the user can select a specific entry and update different values)
 router.put("/editEntry", async (req, res) => {
   UserEntries.update({ 
-    text: 'a very different text update RETURNS'
+    text: 'a very different text update RETURNS THE SEQUEL'
    },
    {
     where: {
-      createdAt: "2022-02-15T23:23:28.000Z"
+      createdAt: `2022-02-15T23:23:28.000Z`
     }
 	}).then(
     UserEntries.findAll().then((userData) => {
-      console.log("showing them all to you with updates!");
+      console.log("showing them all to you with updates!")
       res.json(userData)})
     );
 });
