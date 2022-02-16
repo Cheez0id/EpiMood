c53784f43aa862995006fcd3e603e2c5fd68def7
cheez0idgit <93955021+Chee0id@users.noreply.github.com>
2022-02-16 18:21:08 -0500

    workin' on that form

--- public/script/index.js
+++ public/script/index.js
@@ -1,12 +1,21 @@
 
 
 //this is just testing connection between this file and the index.html. how do I make this talk to tbe backend?
+
+
+
+	function displayRadioValue() {
+		var ele = document.getElementsByName('moodToday');
+			for(i = 0; i < ele.length; i++) {
+				if(ele[i].checked)
+				document.getElementById("result").innerHTML
+								= "MoodToday: "+ele[i].value;
+		}
+}
+
+
 document.getElementById("submitMood").addEventListener("click", function (event) {
 	event.preventDefault();
 	console.log("you clicked the form");
+	displayRadioValue();
 	});
-
-
-
-
-
