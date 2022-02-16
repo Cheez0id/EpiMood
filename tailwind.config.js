01b644ea92c4505697f8a462757bc6fd98efa0fe
cheez0idgit <93955021+Chee0id@users.noreply.github.com>
2022-02-16 10:35:36 -0500

    grabbed a css form example from the interwebs. https://ordinarycoders.com/blog/article/tailwindcss-custom-forms

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+    fontFamily: {
+      display: ['Menlo', 'Courier New"'],
+      body: ['Courier New"', 'monospace'],
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
