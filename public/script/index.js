91c5041256da1bf590255852c1f2ece220f82e92
cheez0idgit <93955021+Chee0id@users.noreply.github.com>
2022-02-16 19:16:04 -0500

    OBJECTS EVERYWHERE!

--- public/script/index.js
+++ public/script/index.js
@@ -1,29 +1,42 @@
-
-
 //this is just testing connection between this file and the index.html. how do I make this talk to tbe backend?
 
-
-
 function displayRadioValue() {
-		let ele = document.getElementsByName('moodToday');
-			for(i = 0; i < ele.length; i++) {
-				if(ele[i].checked)
-				document.getElementById("result").innerHTML
-								= "MoodToday: "+ele[i].value;	
-
-		}
+	let ele = document.getElementsByName("moodToday");
+	for (i = 0; i < ele.length; i++) {
+		if (ele[i].checked)
+			document.getElementById("result").innerHTML =
+				"MoodToday: " + ele[i].value;
+	}
 }
 
-document.getElementById("submitMood").addEventListener("click", function (event) {
-	event.preventDefault();
-	console.log("you clicked the form");
-	displayRadioValue();
+document
+	.getElementById("submitMood")
+	.addEventListener("click", function (event) {
+		event.preventDefault();
+		console.log("you clicked the form");
+		displayRadioValue();
 	});
 
-	document.getElementById("viewAllEntries").addEventListener("click", function (event) {
+
+//attempting to put all entries on the page	
+document
+	.getElementById("viewAllEntries")
+	.addEventListener("click", function (event) {
 		event.preventDefault();
 		console.log("you gonna see it all!");
-		fetch('http://localhost:80/allData')
-		.then(response=> response.json())
-		.then (data => console.log(data))
-		});
+		fetch("http://localhost:80/allData")
+			.then((response) => response.json())
+			.then((data) => document.getElementById("result").innerHTML
+			= data);
+	});
+
+//I would very much like to make a post request.
+	// document
+	// .getElementById("createNewEntry")
+	// .addEventListener("click", function (event) {
+	// 	event.preventDefault();
+	// 	console.log("We'll try and post something");
+	// 	fetch("http://localhost:80/newEntry")
+	// 		.then((response) => response.json())
+	// 		.then((data) => console.log(data));
+	// });
