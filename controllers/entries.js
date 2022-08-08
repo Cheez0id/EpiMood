d8457fa00fd9d0be3d0711808bc77b52950b98f4
Mary Prince <mleeherr@gmail.com>
2022-08-08 12:32:55 -0400

    I think I fixed the refresh button

--- controllers/entries.js
+++ controllers/entries.js
@@ -1,81 +1,82 @@
 const { UserEntries } = require("../models");
 const router = require("express").Router();
-const sequelize=require('sequelize');
+const sequelize = require("sequelize");
 
 //--------------------
 // Routes for a RESTful API!!
 
 //shows all of the userentries data in JSON
 router.get("/allData", async (req, res) => {
-	UserEntries.findAll().then((entryData) => {
-		console.log("Gotta Catch 'Em All!");
-		res.json(entryData);
-	});
+  UserEntries.findAll().then((entryData) => {
+    console.log("Gotta Catch 'Em All!");
+    res.json(entryData);
+  });
 });
 
 //shows a list of entries with episodes
 router.get("/allData/episodes", async (req, res) => {
-	UserEntries.findAll(episodes).then((entryData) => {
-		console.log("Found episodes!");
-		res.json(entryData);
-	});
+  UserEntries.findAll(episodes).then((entryData) => {
+    console.log("Found episodes!");
+    res.json(entryData);
+  });
 });
 
 //shows a list of entries with moods
 router.get("/allData/mood/", async (req, res) => {
-	UserEntries.findAll(mood).then((entryData) => {
-		console.log("Found moods!");
-		res.json(entryData);
-	});
+  UserEntries.findAll(mood).then((entryData) => {
+    console.log("Found moods!");
+    res.json(entryData);
+  });
 });
 
 //Creates a new entry with specified values (TODO:make it so that the user can put whatever they want;RIGHT NOW THE REQ.BODY.INPUTS AREN'T ANYTHING, AND YEAH. NEED TO MAKE THEM THINGS.)
-router.post("/newEntry", async (req, res) => { 
-  console.log('yoooooooooooo ',req.body);
-	UserEntries.create({
-		mood: req.body.mood,
-		episode: req.body.episode,
-		text: req.body.text,
-		makePrivate: req.body.makePrivate,
-	}).then((newEntry) => {
-		console.log("making a new entry");
-    res.status(200).send('a new entry was created')
-	});
+router.post("/newEntry", async (req, res) => {
+  console.log("yoooooooooooo ", req.body);
+  UserEntries.create({
+    mood: req.body.mood,
+    episode: req.body.episode,
+    text: req.body.text,
+    makePrivate: req.body.makePrivate,
+  }).then((newEntry) => {
+    console.log("making a new entry");
+    res.status(200).send("a new entry was created");
+  });
 });
-
 
 // let variablee= `2022-02-15T23:23:28.000Z`;
 
 //Updates a specific entry with a specified value (TODO: make it so that the user can select a specific entry and update different values)
 router.put("/editEntry", async (req, res) => {
-  let variablee = "2022-02-15"+"T23:23:28.000Z"
-  UserEntries.update({ 
-    text: 'a very different text update RETURNS THE SEQUELBOB MAKES HIS ESCAPE WITH SANTA'
-   },
-   {
-    where: {
-      createdAt: `${variablee}`,
+  let variablee = "2022-02-15" + "T23:23:28.000Z";
+  UserEntries.update(
+    {
+      text: "a very different text update RETURNS THE SEQUELBOB MAKES HIS ESCAPE WITH SANTA",
+    },
+    {
+      where: {
+        createdAt: `${variablee}`,
+      },
     }
-	})
-    UserEntries.findAll().then((userData) => {
-      console.log("showing them all to you with updates!")
-      res.json(userData)})
-
+  );
+  UserEntries.findAll().then((userData) => {
+    console.log("showing them all to you with updates!");
+    res.json(userData);
+  });
 });
 
 //Destroys a specified entry by ID (TODO: Make it so that the user can specify which entry to destroy)
 router.delete("/deleteEntry", async (req, res) => {
-	UserEntries.destroy(
-   {
+  UserEntries.destroy({
     where: {},
-	truncate:true
-	}).then(function(){
-       console.log('Deleted everything successfully');
-     }, function(err){
-      console.log(err); 
-  });
+    truncate: true,
+  }).then(
+    function () {
+      console.log("Deleted everything successfully");
+    },
+    function (err) {
+      console.log(err);
+    }
+  );
 });
 
 module.exports = router;
-
-
--- public/script/index.js
+++ public/script/index.js
@@ -3,159 +3,128 @@
 
 //creating an onlonad event with a quick alert box
 window.onload = function () {
-	alert(
-		"!WARNING! This is a SAMPLE app! Anything that you submit is CURRENTLY VISIBLE TO THE PUBLIC!"
-	);
+  alert(
+    "!WARNING! This is a SAMPLE app! Anything that you submit is CURRENTLY VISIBLE TO THE PUBLIC!"
+  );
 };
 
-//The radio selection on the form; currently the function will loop through each radio named 'moodtoday' and will take the checked radio, then put the value into the result area below;
-//TODO: take the radio value and create a variable-? It needs to be able to go into the post function; moodvariable is already created, let's just use that
-function displayRadioValue() {
-	// let ele = document.getElementsByName("moodToday");
-	// for (i = 0; i < ele.length; i++) {
-	// 	if (ele[i].checked)
-	// 		document.getElementById("result").innerHTML =
-	// 			"MoodToday: " + ele[i].value;
-	// 			//this is me trying to export value
-	// 	let moodNumber = ele[i].value;
-	// }
-
-	document.getElementById("result").innerHTML = "MoodToday: " + selectedMood();
+let testRoute = "";
+let allLocalData = "http://localhost:80/allData";
+let allDeployedData = "https://epimoodtracker.herokuapp.com/allData";
+console.log(window.location.href);
+if (window.location.href == "http://localhost/") {
+  testRoute = allLocalData;
+} else {
+  testRoute = allDeployedData;
 }
-
-//moodInputs = [{checked: false, mood: 1}, {checked: true, mood: 2}, {checked: false, mood: 3}]
-	//checkedMood = moodInputs.find(moodInput => moodInput.checked === true);
-	// checkedMood is {checked: true, mood: 2}
-
-	let testRoute ='';
-	let allLocalData = 'http://localhost:80/allData'
-	let allDeployedData = 'https://epimoodtracker.herokuapp.com/allData'
-console.log(window.location.href)
-	if (window.location.href == 'http://localhost/'){
-		testRoute = allLocalData
-	} else {
-		testRoute = allDeployedData
-	}
 
 //Displays all DB entries as 'cards' on the page
 const details = document.getElementById("result");
 document
-	.getElementById("viewAllEntries")
-	.addEventListener("click", function (event) {
-		event.preventDefault();
-		console.log("you gonna see it all!");
-		fetch(testRoute)
-			.then((response) => response.json())
-			// .then((data) => console.log(JSON.stringify(data)))
-			.then(function (data) {
-				for (let i = 0; i < data.length; i++) {
-					let entry = document.createElement("p");
-					entry.className = "card";
-					entry.innerHTML = ` 
+  .getElementById("viewAllEntries")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    console.log("you gonna see it all!");
+    fetch(testRoute)
+      .then((response) => response.json())
+      // .then((data) => console.log(JSON.stringify(data)))
+      .then(function (data) {
+        for (let i = 0; i < data.length; i++) {
+          let entry = document.createElement("p");
+          entry.className = "card";
+          entry.innerHTML = ` 
 					Today's Date/Time ${data[i].createdAt}<br>
 					Note: ${data[i].text} <br> Today's Mood: ${data[i].mood} <br> Episode Today?: ${data[i].episode} <br> 
 				Private? ${data[i].makePrivate}`;
-					details.append(entry);
-				}
+          details.append(entry);
+        }
 
-				//  `Today's Mood: ${data[i].mood} <br>
-				//  Private? ${data[i].makePrivate}`;
-				//  console.log(data[0]);
-				console.log(data);
-			});
-	});
-
-
+        //  `Today's Mood: ${data[i].mood} <br>
+        //  Private? ${data[i].makePrivate}`;
+        //  console.log(data[0]);
+        console.log(data);
+      });
+  });
 
 function selectedMood() {
-	let ele = document.getElementsByName("moodToday");
-	let mood
-	for (i = 0; i < ele.length; i++) {
-		if (ele[i].checked === true){
-			mood = ele[i].value
-		}}
-	return mood;
-	}
+  let ele = document.getElementsByName("moodToday");
+  let mood;
+  for (i = 0; i < ele.length; i++) {
+    if (ele[i].checked === true) {
+      mood = ele[i].value;
+    }
+  }
+  return mood;
+}
 
 const hasEpisode = () => {
-	return document.getElementById('episodeCheckBox').checked
-}
+  return document.getElementById("episodeCheckBox").checked;
+};
 
 const makePrivate = () => {
-	return document.getElementById('makePrivateCheckBox').checked
-}
+  return document.getElementById("makePrivateCheckBox").checked;
+};
 
 const todayNote = () => {
-	return document.getElementById('noteToday').value
-}
+  return document.getElementById("noteToday").value;
+};
 
-
-
-let testRoute2 ='';
-let newLocalEntry = 'http://localhost:80/newEntry'
-let newDeployedEntry = 'https://epimoodtracker.herokuapp.com/newEntry'
-if (window.location.href == 'http://localhost/'){
-	testRoute2 = newLocalEntry
+let newEntryRoute = "";
+let newLocalEntry = "http://localhost:80/newEntry";
+let newDeployedEntry = "https://epimoodtracker.herokuapp.com/newEntry";
+if (window.location.href == "http://localhost/") {
+  newDeployedEntry = newLocalEntry;
 } else {
-	testRoute2 = newDeployedEntry
+  newDeployedEntry = newDeployedEntry;
 }
 
 document
-	.getElementById("createNewEntry")
-	.addEventListener("click", async function (event) {
-		event.preventDefault();
-		await fetch(testRoute2,
-		{ 
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify({ 
-				mood: selectedMood(),
-				episode: hasEpisode(),
-				text: todayNote(),
-				makePrivate: makePrivate()
-			})
-		})
-	});
-
-	document
-	.getElementById("createNewEntry")
-	.addEventListener("click", async function (event) {
-		event.preventDefault();
-	location.reload})
+  .getElementById("createNewEntry")
+  .addEventListener("click", async function (event) {
+    event.preventDefault();
+    await fetch(newDeployedEntry, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        mood: selectedMood(),
+        episode: hasEpisode(),
+        text: todayNote(),
+        makePrivate: makePrivate(),
+      }),
+    });
+  });
 
 
-
-
-
+// this will refresh the page
+  document
+  .getElementById("refreshPage")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    location.reload();
+  
+  });
 
 //trying to delete all
+let deleteRoute = "";
+let deleteLocalData = "http://localhost:80/deleteEntry";
+let deleteDeployedData = "https://epimoodtracker.herokuapp.com/deleteEntry";
+console.log(window.location.href);
+if (window.location.href == "http://localhost/") {
+  deleteRoute = deleteLocalData;
+} else {
+  deleteRoute = deleteDeployedData;
+}
 
-
-
-let testRoute3 ='';
-	let deleteLocalData = 'http://localhost:80/deleteEntry'
-	let deleteDeployedData = 'https://epimoodtracker.herokuapp.com/deleteEntry'
-console.log(window.location.href)
-	if (window.location.href == 'http://localhost/'){
-		testRoute = allLocalData
-	} else {
-		testRoute = allDeployedData
-	}
-
-//Displays all DB entries as 'cards' on the page
 document
-	.getElementById("deleteAll")
-	.addEventListener("click", function (event) {
-		event.preventDefault();
-		console.log("deleteEverything");
-		fetch(testRoute3)
-	});
-
-
-
-// module.exports = { moodNumber };
+  .getElementById("deleteAll")
+  .addEventListener("click", function (event) {
+    console.log("deleteEverything");
+    event.preventDefault();
+    fetch(deleteRoute).then;
+    console.log("deleteEverything");
+  });
 
 // ------------------------------- IGNORE BELOW, IT'S REFERENCE CODE----------------------//
 
@@ -183,3 +152,24 @@ document
 // }
 
 // document.querySelector('.new-dish-form').addEventListener('submit', newFormHandler);
+
+//something about showing moods
+// //The radio selection on the form; currently the function will loop through each radio named 'moodtoday' and will take the checked radio, then put the value into the result area below;
+// //TODO: take the radio value and create a variable-? It needs to be able to go into the post function; moodvariable is already created, let's just use that
+// function displayRadioValue() {
+// 	// let ele = document.getElementsByName("moodToday");
+// 	// for (i = 0; i < ele.length; i++) {
+// 	// 	if (ele[i].checked)
+// 	// 		document.getElementById("result").innerHTML =
+// 	// 			"MoodToday: " + ele[i].value;
+// 	// 			//this is me trying to export value
+// 	// 	let moodNumber = ele[i].value;
+// 	// }
+
+// 	document.getElementById("result").innerHTML = "MoodToday: " + selectedMood();
+// }
+
+//moodInputs = [{checked: false, mood: 1}, {checked: true, mood: 2}, {checked: false, mood: 3}]
+//checkedMood = moodInputs.find(moodInput => moodInput.checked === true);
+// checkedMood is {checked: true, mood: 2}
+// module.exports = { moodNumber };
